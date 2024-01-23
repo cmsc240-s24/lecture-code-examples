0ccf1b8f8789fd884b571b64085d2935b2331cf9
DavidBalash <david.balash@richmond.edu>
2024-01-22 22:20:11 -0500

    Added hello.pp

---
+++ 02-lecture/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cout << "Please enter your first name (followed by 'enter'):" << endl;
+    string first_name;     // first_name variable of type string
+    cin >> first_name;     // read characters into first_name
+    cout << "Hello, " << first_name << "!" << endl;
+    return 0;
+}
